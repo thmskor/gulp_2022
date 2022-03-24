f27758d7a1ca087fad6b5ebeee3929a5d359d1f6
ThmsKor <korchagin.art@gmail.com>
2022-03-24 22:24:11 +0600

    2

---
+++ gulpfile.js
@@ -0,0 +1,25 @@
+import gulp from "gulp";
+import { path } from "./gulp/config/path.js";
+import { plugins } from "./gulp/config/plugins.js";
+
+global.app = {
+    path: path,
+    gulp: gulp,
+    plugins: plugins
+}
+
+import { copy } from "./gulp/tasks/copy.js"
+import { reset } from "./gulp/tasks/reset.js"
+import { html } from "./gulp/tasks/html.js"
+
+function watcher() {
+    gulp.watch(path.watch.files, copy);
+    gulp.watch(path.watch.html, html)
+
+}
+
+const mainTasks = gulp.parallel(copy, html);
+
+const dev = gulp.series(reset, mainTasks, watcher)
+
+gulp.task('default', dev);
